a85695f2215512dd1e4848f342d241dd4f0af2b7
Blake Dy <blake@walla.by>
2015-08-26 18:18:18 -0700

    init yo angular skeleton

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name foosballSlackApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the foosballSlackApp
+ */
+angular.module('foosballSlackApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name foosballSlackApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the foosballSlackApp
+ */
+angular.module('foosballSlackApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
